a08594c21c6383ed32385596185431d481f723b1
nth-coding <sadtearchelsea@gmail.com>
2022-08-10 18:22:44 +0700

    color changed

--- sanpham.php
+++ sanpham.php
@@ -24,40 +24,6 @@
     	</div>
 	</section>
 
-	<section id="hero">
-		<h4> Trade-in-offer </h4>
-		<h2> Super value deals </h2>
-		<h1> On all products </h1>
-		<p> Save more with coupons & up to 70% off! </p>
-		<a href="sanpham.php"> Shop now </a>
-	</section>
-
-	<section id="feature" class="section-p1">
-		<div class="fe-box">
-			<img src="imgs/features/f1.png" alt="">
-			<h6> Free Shipping </h6> 
-		</div>
-		<div class="fe-box">
-			<img src="imgs/features/f2.png" alt="">
-			<h6> Online Order </h6> 
-		</div>
-		<div class="fe-box">
-			<img src="imgs/features/f3.png" alt="">
-			<h6> Save Money </h6> 
-		</div>
-		<div class="fe-box">
-			<img src="imgs/features/f4.png" alt="">
-			<h6> Promotions </h6> 
-		</div>
-		<div class="fe-box">
-			<img src="imgs/features/f5.png" alt="">
-			<h6> Happy Sell </h6> 
-		</div>
-		<div class="fe-box">
-			<img src="imgs/features/f6.png" alt="">
-			<h6> F24/7 Support </h6> 
-		</div>
-	</section>
 
 	<div class="wrapper">
     	<?php
