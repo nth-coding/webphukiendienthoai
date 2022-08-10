79876bf8ef97a1895f0a05a1c2b1cbc5f04554c7
nth-coding <sadtearchelsea@gmail.com>
2022-08-11 00:48:28 +0700

    lol

--- modules/right/spmoi.php
+++ modules/right/spmoi.php
@@ -42,7 +42,7 @@
 				while ($dong = mysql_fetch_array($row)) {
 				?>
 				<div class="pro">
-					<li><a href="?quanly=chitietsp&idloaisp=<?php echo $dong['loaisp'] ?>&id=<?php echo $dong['idsanpham'] ?>">
+					<li><a href="sanpham.php?quanly=chitietsp&idloaisp=<?php echo $dong['loaisp'] ?>&id=<?php echo $dong['idsanpham'] ?>">
 							<img src="admincp/modules/quanlysanpham/uploads/<?php echo $dong['hinhanh'] ?>" width="150" height="150" />
 							<p style="color:skyblue"><?php echo $dong['tensp'] ?></p>
 							<p style="color:#088178;font-weight:bold; border:1px solid #d9d9d9; width:150px;
